3a45673460f74cb65b347f5faf11b433b4b84fe0
lennoncc <lencruz@ucdavis.edu>
2021-12-13 17:12:51 -0800

    day 3 part 2 done

--- day3/test.cpp
+++ day3/test.cpp
@@ -8,6 +8,34 @@
 
 using namespace std;
 
+typedef struct {
+    int zero;//[1000];
+    int one;//[1000];
+  } bit;
+
+int findmostcommon(vector<string> lines, bit zero, int index) {
+  // for(int i = 0; i < 5; i++) {
+    for(int j = 0; j < lines.size(); j++) {
+      if(lines[j][index] - '0' == 1) {
+        zero.one++;
+      } else {
+        zero.zero++;
+      }
+    }
+    cout << "# of zeroes: " << zero.zero << endl;
+    cout << "# of ones: " << zero.one << endl;
+    if(zero.zero > zero.one) {
+      cout << "returning zero\n";
+      return 0;
+    } else {
+      cout << "returning one\n";
+      return 1;
+    }
+    zero.zero = 0;
+    zero.one = 0;
+  // }
+}
+
 int main() {
   string line;
   int bit0;  
@@ -22,10 +50,7 @@ int main() {
   int bit9;
   int bit10;
   int bit11;
-  typedef struct {
-    int zero;//[1000];
-    int one;//[1000];
-  } bit;
+  
   bit zero;
   zero.zero = 0;
   zero.one = 0;
@@ -33,26 +58,29 @@ int main() {
     // zero.zero[i] = 0;
   // }
   vector<string> lines;  
-  ifstream myfile ("test.txt");
+  ifstream myfile ("day3.txt");
   if (myfile.is_open()) {
     while (getline(myfile,line)) {
       lines.push_back(line);
     }
   }
-  
-  vector<int> answer;
-  answer.push_back(1);
-  answer.push_back(0);
-  answer.push_back(1);
-  answer.push_back(1);
-  answer.push_back(1);
 
-  // cout << "size is " << lines.size() << endl;
-  for(int i = 0; i < 5; i++) {
+  // findmostcommon(lines, zero);
+
+  // vector<int> answer;
+  // answer.push_back(1);
+  // answer.push_back(0);
+  // answer.push_back(1);
+  // answer.push_back(1);
+  // answer.push_back(1);
+  int newbit;
+  // // cout << "size is " << lines.size() << endl;
+  for(int i = 0; i < 12/*5*/; i++) {
     cout << "checking bit #: " << i + 1 << endl;
+    newbit = findmostcommon(lines,zero, i);
     for(int j = 0; j < lines.size()/*12*/; j++) {
       cout << "checking element #" << j << ": " << lines[j] << endl;
-      if((lines[j][i] - '0' != answer[i])) {
+      if((lines[j][i] - '0' != newbit/*answer[i]*/)) {
         if(lines.size() == 1) {
           break;
         }
